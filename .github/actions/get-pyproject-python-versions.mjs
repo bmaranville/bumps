642c3e6648f6154b3bde1504550de2e3defcc8d9
Brian Benjamin Maranville <brian.maranville@nist.gov>
2025-08-29 10:50:34 -0400

    Add action to parse Python versions from pyproject.toml

---
+++ .github/actions/get-pyproject-python-versions.mjs
@@ -0,0 +1,42 @@
+import { readFileSync } from "fs";
+import assert from "assert";
+import { setOutput, setFailed } from "core";
+
+function parsePyproject() {
+  try {
+    const pyprojectToml = readFileSync('pyproject.toml', 'utf8');
+    const requiresMatch = pyprojectToml.match(/requires-python\s*=\s*['"]([^'"]+)['"]/);
+    if (!requiresMatch) {
+      throw new Error("no reuires-python key-value found");
+    }
+    const requiresPython = requiresMatch[1];
+    const minVerMatch = requiresPython.match(/>=(\d+)\.(\d+)/);
+    if (!minVerMatch) {
+      throw new Error("No minimum 'python-requires' version specified in pyproject.toml with '>='")
+    }
+    const maxVerMatch = requiresPython.match(/<=(\d+)\.(\d+)/);
+    if (!maxVerMatch) {
+      throw new Error("No maximum 'python-requires' version specified in pyproject.toml with '<='");
+    }
+    if (minVerMatch[1] !== maxVerMatch[1]) {
+      throw new Error(`major version of 'requires-python' min (${minVerMatch[0]}) and max (${maxVerMatch[0]}) don't agree`);
+    }
+    const minMajor = +minVerMatch[1];
+    const minMinor = +minVerMatch[2];
+    const maxMinor = +maxVerMatch[2];
+    assert(maxMinor >= minMinor, "max version is not greater than or equal to min version");
+    let minor = minMinor;
+    const versions = [];
+    while (minor <= maxMinor) {
+      versions.push(`${minMajor}.${minor}`);
+      minor++;
+    }
+    setOutput("versions", JSON.stringify(versions));
+    setOutput("min_python", versions.at(0));
+    setOutput("max_python", versions.at(-1));
+  } catch (error) {
+    setFailed(error.message);
+  }
+}
+
+export default parsePyproject;
